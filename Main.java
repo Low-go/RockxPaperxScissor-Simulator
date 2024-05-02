ca983fe44c15ec45d5fe8f945818b7e4727de332
lorran <lorranalvesgaldino98@gmail.com>
2024-05-01 15:59:58 -1000

    add asset images. frame/window for game, rock class with image.

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.util.Timer;
+
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        JFrame frame = new JFrame("Window");
+        frame.setSize(750, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
+}
+
+//inside here that the game should occur, paint components will draw, so this will be added in the frame
+class MyPanel extends JPanel{
+    
 }
---
+++ Rock.java
@@ -0,0 +1,11 @@
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+
+public class Rock {
+    
+    public Rock(){
+        ImageIcon icon = new ImageIcon("C:\\Users\\lorran\\Documents\\RPS_eletric_boogalo\\assets\\rock.png");
+    }
+}
